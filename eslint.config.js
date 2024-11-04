4bb43f772d51fb79f9b693f7a16cd9d374fe71ce
sumin <sumin@pia.space>
2024-11-04 22:03:51 +0900

    fix: 'React' must be in scope when using jsx EsLint 에러 수정
    - 'React' must be in scope when using jsx 오류 수정

--- eslint.config.js
+++ eslint.config.js
@@ -9,5 +9,5 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
 ];
